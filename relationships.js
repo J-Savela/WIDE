3b0cdc6e7cca59e36b0b16f0222c7688bdc051e2
Janne555 <Janne555@users.noreply.github.com>
2018-10-27 16:01:55 +0300

    i dont know what to write here

--- relationships.js
+++ relationships.js
@@ -14,21 +14,101 @@ function getFromWeb(url) {
     return response;
 }
 
+function getUriForWord(word) {
+    let url = "http://api.finto.fi/rest/v1/search?vocab=yso&query=" + word + "&lang=fi";
+    let response = getFromWeb(url);
+    return response.results[0].uri;
+}
+
+function goBroader(uri) {
+    let url = "http://api.finto.fi/rest/v1/yso/broader?uri=" + uri;
+    let response = getFromWeb(url);
+    return response.broader;
+}
+
+function goNarrower(uri) {
+    let url = "http://api.finto.fi/rest/v1/yso/narrower?uri=" + uri;
+    let response = getFromWeb(url);
+    return response;
+}
+
+function getSiblingWords(word) {
+    try {
+    let array = [];
+    let wordUri = getUriForWord(word);
+    let broaderArray = goBroader(wordUri);
+    for (let i in broaderArray) {
+        let broaderUri = broaderArray[i].uri;
+        let siblings = goNarrower(broaderUri);
+        let mapped = siblings.narrower.map(function(word) { return word.prefLabel });
+        mapped = mapped.filter(sibling => word !== sibling);
+        array = [...array, ...mapped];
+    }
+
+    let jsonObj = {"subject": word, "siblings": array};
+    return jsonObj;
+    } catch (err) {
+        console.error(err);
+        return({"subject": word, "siblings":[]})
+    }
+}
+
+
 function pairCount(word1, word2) {
     let urlOr = "https://api.finna.fi/v1/search?bool0[]=OR&lookfor0[]=" + word1 + "&lookfor0[]=" + word2 + "&limit=0";
     let urlAnd = "https://api.finna.fi/v1/search?bool0[]=AND&lookfor0[]=" + word1 + "&lookfor0[]=" + word2 + "&limit=0";
-    let responseOr = getFromWeb(urlOr);
-    let responseAnd = getFromWeb(urlAnd);
-    return ({"word1": word1, "word2": word2, "ORcount": responseOr.resultCount, "ANDcount": responseAnd.resultCount});
+    let responseOr = getFromWeb(urlOr).resultCount;
+    let responseAnd = getFromWeb(urlAnd).resultCount;
+    let ratio = Math.floor(responseAnd / responseOr * 100);
+
+    return ({"word1": word1, "word2": word2, "ORcount": responseOr, "ANDcount": responseAnd, "ratio": ratio});
 }
 
-function pairs(siblingwords) {
+function hitsforOriginal(word1, word1ResultCount, word2) {
+    let urlAnd = "https://api.finna.fi/v1/search?bool0[]=AND&lookfor0[]=" + word1 + "&lookfor0[]=" + word2 + "&limit=0";
+    let responseAnd = getFromWeb(urlAnd).resultCount;
+    let ratio = responseAnd / word1ResultCount;
+
+    return ({"word1": word1, "word2": word2, "Word1Count": word1ResultCount, "ANDcount": responseAnd, "ratio": ratio});
+}
+
+function pairs(siblingwords, limit) {
     let array = [];
-    for (let i = 0; i < siblingwords.length; i++) {
-        for (let j = i + 1; j < siblingwords.length; j++) {
+    if (siblingwords.length < limit) {
+        limit = siblingwords.length;
+    }
+    for (let i = 0; i < limit; i++) {
+        for (let j = i + 1; j < limit; j++) {
             let result = pairCount(siblingwords[i], siblingwords[j]);
             console.log(result);
         }
     }
     return array;
+}
+
+function resultCountForWord(word) {
+    let url = "https://api.finna.fi/v1/search?&lookfor=" + word + "&limit=0";
+    let response = getFromWeb(url);
+    return response.resultCount;
+}
+
+
+function relationToMain(stuff) {
+    let subject = stuff.subject;
+    let resultCountForSubject = resultCountForWord(subject);
+    let siblings = stuff.siblings;
+    let array = [];
+    for (let i in siblings) {
+        let results = hitsforOriginal(subject, resultCountForSubject, siblings[i]);
+        array.push(results);
+    }
+    array.sort(function(a,b){
+        return parseFloat(a.ratio) - parseFloat(b.ratio);
+    });
+    return array;
+}
+
+function redbul(word) {
+    let words = getSiblingWords(word);
+    return relationToMain(words);
 }
